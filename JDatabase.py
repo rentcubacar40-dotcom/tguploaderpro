1d9d2b8cc88454206ef78188e6cf106998e33fb2
Eliel <rentcubacar40@gmail.com>
2025-11-28 04:58:27 -0500

    Update JDatabase.py

--- JDatabase.py
+++ JDatabase.py
@@ -2,7 +2,6 @@ import os
 import json
 import time
 import io
-from datetime import datetime
 
 class JsonDatabase(object):
     def __init__(self,path='newdb'):
@@ -28,48 +27,30 @@ class JsonDatabase(object):
         dbfile.close()
 
     def create_user(self,name):
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.items[name] = {
-            'dir': '',
-            'cloudtype': 'moodle',
-            'moodle_host': '',
-            'moodle_repo_id': 4,
-            'moodle_user': '',
-            'moodle_password': '',
-            'isadmin': 0,
-            'zips': 100,
-            'uploadtype':'draft',
-            'proxy':'',
-            'tokenize':0,
-            # NUEVOS CAMPOS DE ESTADÍSTICAS
-            'upload_count': 0,
-            'total_mb_used': 0,
-            'last_upload': '',
-            'first_upload': '',
-            'account_created': current_time
-        }
+        self.items[name] = {'dir': '',
+                     'cloudtype': 'moodle',
+                     'moodle_host': '',
+                     'moodle_repo_id': 4,
+                     'moodle_user': '',
+                     'moodle_password': '',
+                     'isadmin': 0,
+                     'zips': 100,
+                     'uploadtype':'draft',
+                     'proxy':'',
+                     'tokenize':0}
 
     def create_admin(self,name):
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.items[name] = {
-            'dir': '',
-            'cloudtype': 'moodle',
-            'moodle_host': 'https://aulacened.uci.cu/',
-            'moodle_repo_id': 5,
-            'moodle_user': 'eliel21',
-            'moodle_password': 'ElielThali2115.',
-            'isadmin': 1,
-            'zips': 100,
-            'uploadtype':'draft',
-            'proxy':'',
-            'tokenize':0,
-            # NUEVOS CAMPOS DE ESTADÍSTICAS
-            'upload_count': 0,
-            'total_mb_used': 0,
-            'last_upload': '',
-            'first_upload': '',
-            'account_created': current_time
-        }
+        self.items[name] = {'dir': '',
+                     'cloudtype': 'moodle',
+                     'moodle_host': 'https://aulacened.uci.cu/',
+                     'moodle_repo_id': 5,
+                     'moodle_user': 'eliel21',
+                     'moodle_password': 'ElielThali2115.',
+                     'isadmin': 1,
+                     'zips': 100,
+                     'uploadtype':'draft',
+                     'proxy':'',
+                     'tokenize':0}
 
     def remove(self,name):
         try:
@@ -83,7 +64,6 @@ class JsonDatabase(object):
             return None
 
     def save_data_user(self,user, data):
-        # SOLO GUARDA LOS DATOS, NO AGREGA NADA
         self.items[user] = data
 
     def is_admin(self,user):
@@ -91,14 +71,6 @@ class JsonDatabase(object):
         if User:
             return User['isadmin'] == 1
         return False
-
-    def get_all_users(self):
-        """Obtiene todos los usuarios registrados"""
-        return self.items
-
-    def get_user_count(self):
-        """Obtiene el número total de usuarios"""
-        return len(self.items)
 
     def load(self):
         dbfile = open(self.path, 'r')
@@ -109,4 +81,5 @@ class JsonDatabase(object):
             tokens = lin.split('=')
             user = tokens[0]
             data = json.loads(str(tokens[1]).replace("'", '"'))
+
             self.items[user] = data
